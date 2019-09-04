dbcbcff4458d7a36da8a20bf16295634f27488ec
Kattenelvis <officialkattenelvis@gmail.com>
2019-09-04 22:11:30 +0200

    Bubble Sort and vizualization is now 100% function

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
+let inputArray = [];
 let newArray = [];
 let recordedChanges = [];
 
-const init = () => {
-  for (let i = 0; i < 20; i++) {
-    let height = parseInt(Math.random() * 600);
-    newArray.push({height:height, pos:i});
+const randomArray = () =>{
+    for (let i = 0; i < 20; i++) {
+        inputArray.push(parseInt(Math.random()*600))
 
-    let line = document.createElement("div");
-    line.classList = "line";
-    line.style = `height:${height}px; margin-left:2px;`;
-    line.innerText = height;
-    document.getElementsByClassName("line-container")[0].appendChild(line);
+    }
+}
+
+const init = arr => {
+    document.getElementsByClassName("line-container")[0].innerHTML = ''
+    for (let i = 0; i < 20; i++) {
+
+        let height = arr[i];
+        newArray.push({ height: height, pos: i });
+
+        let line = document.createElement("div");
+        line.classList = "line";
+        line.style = `height:${height}px; order:${i}`;
+        //line.innerText = height;
+        line.id = `line-${i}`;
+        document.getElementsByClassName("line-container")[0].appendChild(line);
   }
 };
 
@@ -19,38 +30,36 @@ const scan = () => {};
 const swap = () => {};
 
 const bubbleSort = arr => {
-    let arrInt=[]
-     arr.forEach(line =>{
-        arrInt.push(line.height)
-    })
-  for (let i = 0; i < arrInt.length; i++) {
-    for (let j = 0; j < arrInt.length - i - 1; j++) {
-
-      if (arrInt[j] > arrInt[j+1]){
-        recordedChanges.push(arr[j]) 
-        [arrInt[j], arrInt[j+1]] = [arrInt[j+1], arrInt[j]]
-
-      }
+  let arrInt = []
+    arr.forEach(line => {
+      arrInt.push(line.height);
+    });
+    for (let i = 0; i < arrInt.length; i++) {
+        for (let j = 0; j < arrInt.length - i ; j++) {
+        if (arrInt[j] > arrInt[j + 1]) {
+            [arrInt[j], arrInt[j + 1]] = [arrInt[j + 1], arrInt[j]];
+            recordedChanges.push(JSON.parse(JSON.stringify(arrInt)));
+            }
+        }
     }
-  }
-  console.log(arrInt)
+    return arrInt
 };
 
 const interval = () => {
-  setInterval(() => {
-  }, 500);
+    let i = 0;
+    let lines = document.getElementsByClassName("line")
+    console.log(recordedChanges)
+    let interval = setInterval(() => {
+
+        //init(recordedChanges[i].map(({height})=> height))
+        init(recordedChanges[i])
+        if (recordedChanges[i+1] === undefined) clearInterval(interval)
+        i++
+    }, 20);
 };
 
-init();
-bubbleSort(JSON.parse(JSON.stringify(newArray)));
+randomArray()
+init(inputArray);
+let sortedArray = bubbleSort(JSON.parse(JSON.stringify(newArray)));
 
 interval();
-
-
-
-
-
-
-//let num = parseInt(document.getElementsByClassName("line")[j].innerText);
-//if (num > maxNum) {
-// maxNum = num;
