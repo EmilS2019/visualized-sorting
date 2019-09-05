c80aeab1ee0df0a089ddce498128307f5bf9cf0f
Kattenelvis <officialkattenelvis@gmail.com>
2019-09-05 20:03:23 +0200

    Added Selection Sort

--- index.js
+++ index.js
@@ -5,70 +5,44 @@ let recordedChanges = [];
 const randomArray = () =>{
     for (let i = 0; i < 20; i++) {
         inputArray.push(parseInt(Math.random()*600))
-
     }
 }
 
 const init = arr => {
     document.getElementsByClassName("line-container")[0].innerHTML = ''
     for (let i = 0; i < 20; i++) {
-
         let height = arr[i];
         newArray.push({ height: height, pos: i });
 
         let line = document.createElement("div");
         line.classList = "line";
         line.style = `height:${height}px; order:${i}`;
-        //line.innerText = height;
         line.id = `line-${i}`;
         document.getElementsByClassName("line-container")[0].appendChild(line);
   }
 };
 
-const scan = () => {};
-
-const swap = () => {};
-
-const bubbleSort = arr => {
-  let arrInt = []
-    arr.forEach(line => {
-      arrInt.push(line.height);
-    });
-    for (let i = 0; i < arrInt.length; i++) {
-        for (let j = 0; j < arrInt.length - i ; j++) {
-        if (arrInt[j] > arrInt[j + 1]) {
-            [arrInt[j], arrInt[j + 1]] = [arrInt[j + 1], arrInt[j]];
-            recordedChanges.push(JSON.parse(JSON.stringify(arrInt)));
-            }
-        }
-    }
-    return arrInt
-};
-
 const Vizualisation = () => {
     let i = 0;
-    let lines = document.getElementsByClassName("line")
     let interval = setInterval(() => {
         init(recordedChanges[i])
         if (recordedChanges[i+1] === undefined) clearInterval(interval)
         i++
-    }, 20);
+    }, 200);
 };
-
-const handleSubmit = (event) =>{
-    event.preventDefault()
-}
-
-
 
 const options = document.getElementById("options")
 const radio = options.children
 document.getElementById("options").onsubmit = function(e){
     e.preventDefault()
-    
-    if (radio[1].checked){
+    options[0].setAttribute("disabled", true)
 
-        bubbleSort(JSON.parse(JSON.stringify(newArray)));
+    if (radio[1].checked){
+        bubbleSort(newArray);
+        Vizualisation();
+    }
+    else if (radio[2].checked){
+        selectionSort(newArray);
         Vizualisation();
     }
 
---
+++ sortAlgo.js
@@ -0,0 +1,34 @@
+const bubbleSort = arr => {
+    arr = JSON.parse(JSON.stringify(arr))
+    let arrInt = []
+    arr.forEach(line => arrInt.push(line.height));
+
+    for (let i = 0; i < arrInt.length; i++) {
+        for (let j = 0; j < arrInt.length - i ; j++) {
+        if (arrInt[j] > arrInt[j + 1]) {
+            [arrInt[j], arrInt[j + 1]] = [arrInt[j + 1], arrInt[j]];
+            recordedChanges.push(JSON.parse(JSON.stringify(arrInt)));
+            }
+        }
+    }
+    return arrInt
+  };
+
+
+const selectionSort = arr =>{
+    let arrInt = []
+    arr.forEach(line => arrInt.push(line.height));
+
+    let max = 0;
+    for (let i = 0; i < arrInt.length; i++) {
+        
+        for (let j = 0; j < arrInt.length - i; j++) {
+            if (arrInt[j] > arrInt[max]) max = j        
+        }
+
+        [arrInt[arrInt.length-i-1], arrInt[max]] = [arrInt[max], arrInt[arrInt.length-i-1]]
+        recordedChanges.push(JSON.parse(JSON.stringify(arrInt)));
+        max = 0;
+    }
+    return arrInt
+}
