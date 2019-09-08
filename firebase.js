f0d87b9d5ae31abeb143de0dd7be702c4ecbc6f0
Kattenelvis <officialkattenelvis@gmail.com>
2019-09-08 20:07:36 +0200

    Official Launch

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC3UjRufcw7LxnvJFA1xbTk9-Q8QaLwXcw",
+    authDomain: "sorting-visualizer.firebaseapp.com",
+    databaseURL: "https://sorting-visualizer.firebaseio.com",
+    projectId: "sorting-visualizer",
+    storageBucket: "",
+    messagingSenderId: "615535199554",
+    appId: "1:615535199554:web:7d3dc6c6ff58ff12f934db"
+  };
+  firebase.initializeApp(firebaseConfig);
